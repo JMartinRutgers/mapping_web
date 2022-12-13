c0ad2c100dea442e1ccfd5bf18afd548f7a45b98
Jorge Melchor <48293166+JMartinRutgers@users.noreply.github.com>
2022-12-13 11:15:10 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibWFydGlucmV2b2x1dGlvbjIwMjUiLCJhIjoiY2tuZXpraGE5MDNmNzJwcDBqZmdteHF4MCJ9.Nc7vlZTF-RnHbTC87fs_Og";
---
+++ logic.js
@@ -0,0 +1,33 @@
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+// var myMap = L.map("map", {
+//   center: [45.52, -122.67],
+//   zoom: 13
+// });
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.streets",
+//   accessToken: API_KEY
+// }).addTo(myMap);
+
+var map = L.map('map').setView([45.52, -122.67], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([45.52, -122.67]).addTo(map)
+    .bindPopup('fucking shit <br> this is fcuk up.')
+    .openPopup();
+
+// original link
+// https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}
+
+
+// Anothet link
+// https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw
